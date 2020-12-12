61d7f4fc030d07345c444622a9528d3c76450357
duytm <sotatekthree@ambergroup.io>
2020-12-12 22:33:33 +0700

    Pizza

--- src/routes/route.js
+++ src/routes/route.js
@@ -30,6 +30,12 @@ router.post('/index', (req, res) => {
         age: 27
     };
     res.status(200).json(user);
+});
+
+router.get('/home', (req, res)=> {
+    res.send.json({
+        food: "Pizza"
+    });
 })
 
 module.exports = router;
